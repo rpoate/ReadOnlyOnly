23b8a6b620f0aaf51aab6f439acbdaf7d0c8fe29
Richard Poate <info@zoople.com.au>
2022-11-07 00:30:58 +1100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ReadOnlyOnly
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.printToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.copyToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.htmlEditControl1 = new Zoople.HTMLEditControl();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.toolStrip1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.htmlEditControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 25;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printToolStripButton,
+            this.toolStripSeparator,
+            this.copyToolStripButton,
+            this.toolStripSeparator1,
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // printToolStripButton
+            // 
+            this.printToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.printToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("printToolStripButton.Image")));
+            this.printToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.printToolStripButton.Name = "printToolStripButton";
+            this.printToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.printToolStripButton.Text = "&Print";
+            this.printToolStripButton.Click += new System.EventHandler(this.printToolStripButton_Click);
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // copyToolStripButton
+            // 
+            this.copyToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.copyToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("copyToolStripButton.Image")));
+            this.copyToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.copyToolStripButton.Name = "copyToolStripButton";
+            this.copyToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.copyToolStripButton.Text = "&Copy";
+            this.copyToolStripButton.Click += new System.EventHandler(this.copyToolStripButton_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // htmlEditControl1
+            // 
+            this.htmlEditControl1.AcceptsReturn = true;
+            this.htmlEditControl1.AvailableInlineStyles = ((System.Collections.Generic.List<string>)(resources.GetObject("htmlEditControl1.AvailableInlineStyles")));
+            this.htmlEditControl1.BaseURL = null;
+            this.htmlEditControl1.CleanMSWordHTMLOnPaste = true;
+            this.htmlEditControl1.CodeEditor.Enabled = true;
+            this.htmlEditControl1.CodeEditor.Locked = false;
+            this.htmlEditControl1.CodeEditor.TabWidth = 720;
+            this.htmlEditControl1.CodeEditor.WordWrap = false;
+            this.htmlEditControl1.CSSText = null;
+            this.htmlEditControl1.CustomColorPalette = null;
+            this.htmlEditControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.htmlEditControl1.DocumentHTML = null;
+            this.htmlEditControl1.DuplicateBasicTextFormattingAfterEnterKey = false;
+            this.htmlEditControl1.EnableInlineSpelling = false;
+            this.htmlEditControl1.FontSizesList = null;
+            this.htmlEditControl1.FontsList = null;
+            this.htmlEditControl1.HiddenButtons = null;
+            this.htmlEditControl1.HideDOMToolbar = true;
+            this.htmlEditControl1.HideMainToolbar = true;
+            this.htmlEditControl1.ImageStorageLocation = null;
+            this.htmlEditControl1.InCodeView = false;
+            this.htmlEditControl1.LanguageFile = null;
+            this.htmlEditControl1.LicenceActivationKey = null;
+            this.htmlEditControl1.LicenceKey = null;
+            this.htmlEditControl1.LicenceKeyInlineSpelling = null;
+            this.htmlEditControl1.Location = new System.Drawing.Point(0, 0);
+            this.htmlEditControl1.Name = "htmlEditControl1";
+            this.htmlEditControl1.Size = new System.Drawing.Size(800, 421);
+            this.htmlEditControl1.SpellingAutoCorrectionList = ((System.Collections.Hashtable)(resources.GetObject("htmlEditControl1.SpellingAutoCorrectionList")));
+            this.htmlEditControl1.TabIndex = 0;
+            this.htmlEditControl1.ToolstripImageScalingSize = new System.Drawing.Size(16, 16);
+            this.htmlEditControl1.UseParagraphAsDefault = true;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(73, 22);
+            this.toolStripLabel1.Text = "Zoom: 100%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private Zoople.HTMLEditControl htmlEditControl1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton printToolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripButton copyToolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadOnlyOnly
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.htmlEditControl1.DocumentLoadComplete += HtmlEditControl1_DocumentLoadComplete;
+            this.htmlEditControl1.MouseUp += HtmlEditControl1_MouseUp;
+            this.htmlEditControl1.ZoomLevelChanged += HtmlEditControl1_ZoomLevelChanged;
+            this.htmlEditControl1.KeyDown += HtmlEditControl1_KeyDown;
+
+            this.htmlEditControl1.DocumentHTML = "<h2>The simplest and most extensible Winforms HTML Editor.</h2><img src='https://zoople.tech/img/logo.png?1' /><p>" +
+                "Caters for all levels of end-user, from the most basic to people with advanced HTML knowledge.</p>" +
+                "<p>Complete HTML DOM manipulation is achievable, simply and without the need for COM references<a href='https://google.com'>Google Link</a></p>" +
+                "<table width=\"100%\" style=\"border: 3px dotted green\"><tr><td>test1</td></tr><td>test1</td></tr><td>test1</td></tr></table><br />" +
+                "<table width=\"100%\"><tr><td>test1</td><td>test1</td><td>test1</td></tr><td>test1</td><td>test1</td><td>test1</td></tr><td>test1</td><td>test1</td><td>test1</td></tr></table>";
+
+            this.htmlEditControl1.CSSText = "body {font-family: Calibri} table, th, tr, td {border: none !important;}";
+            this.htmlEditControl1.ZoomLevel = 100;
+
+        }
+
+        private void HtmlEditControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control == false) e.SuppressKeyPress = true;
+
+            if (e.KeyCode == Keys.V || e.KeyCode == Keys.X) e.SuppressKeyPress = true;
+
+            if (e.KeyCode == Keys.Oemplus) toolStripButton1_Click(null, null);
+
+            if (e.KeyCode == Keys.OemMinus) toolStripButton2_Click(null, null);
+
+            if (e.KeyCode == Keys.F) this.htmlEditControl1.ShowFindTextDialog();
+
+        }
+
+        private void HtmlEditControl1_ZoomLevelChanged(object sender, EventArgs e)
+        {
+            this.toolStripLabel1.Text = "Zoom: " + this.htmlEditControl1.ZoomLevel + "%";
+        }
+
+        private void HtmlEditControl1_MouseUp(object sender, MouseEventArgs e)
+        {
+            var oEle = this.htmlEditControl1.FindParentElementOfType("a");
+
+            if (oEle != null)
+            {
+                Process browserProc = new Process();
+                browserProc.StartInfo.UseShellExecute = true;
+                browserProc.StartInfo.FileName = oEle.GetAttribute("href");
+                browserProc.Start();
+            }
+        }
+
+        private void HtmlEditControl1_DocumentLoadComplete(object sender, EventArgs e)
+        {
+            this.htmlEditControl1.Document.Body.SetAttribute("contenteditable", "false");
+        }
+
+        private void copyToolStripButton_Click(object sender, EventArgs e)
+        {
+            this.htmlEditControl1.copy_document();
+        }
+
+        private void printToolStripButton_Click(object sender, EventArgs e)
+        {
+            this.htmlEditControl1.print_Preview();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (this.htmlEditControl1.ZoomLevel > 275) return;
+
+            this.htmlEditControl1.ZoomLevel += 25;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (this.htmlEditControl1.ZoomLevel < 50) return;
+
+            this.htmlEditControl1.ZoomLevel -= 25;
+        }
+    }
+}
